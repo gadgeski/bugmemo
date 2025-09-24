15787fe6c8ce464e1dce688a33a18c01e4426a56
gadgeski <xtwh.ce5bil@gmail.com>
2025-09-24 09:56:01 +0900

    feat: (bugmemo/app/build.gradle.kts)

---
+++ app/build.gradle.kts
@@ -0,0 +1,64 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+}
+
+android {
+    namespace = "com.example.bugmemo"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.example.bugmemo"
+        minSdk = 34
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17   // ★ Changed
+        targetCompatibility = JavaVersion.VERSION_17   // ★ Changed
+    }
+    kotlin {
+        jvmToolchain(17)   // ★ Added
+    }
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.activity.compose) // setContent{} 用
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.compose.ui) // UI 基本
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.compose.material3) // Material 3
+    // ★ Compose: 画面遷移・Lifecycle/ViewModel 連携
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.lifecycle.runtime.compose)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+    debugImplementation(libs.androidx.compose.ui.tooling)
+    debugImplementation(libs.androidx.compose.ui.test.manifest)
+    // runtime-ktx は使っていれば↓を残す、使っていなければ削除
+// implementation(libs.androidx.lifecycle.runtime.ktx)
+}
