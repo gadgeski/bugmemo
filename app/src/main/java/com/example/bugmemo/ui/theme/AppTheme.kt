17e386d6cf35a2cae4b1eee3eae5107dc2166683
gadgeski <xtwh.ce5bil@gmail.com>
2025-09-25 09:18:12 +0900

    feat:(アプリ全体の見た目一括管理)

---
+++ app/src/main/java/com/example/bugmemo/ui/theme/AppTheme.kt
@@ -0,0 +1,12 @@
+// ----------------------------------------
+// file: app/src/main/java/com/example/bugmemo/ui/theme/AppTheme.kt
+// ----------------------------------------
+package com.example.bugmemo.ui.theme
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+
+@Composable
+fun AppTheme(content: @Composable () -> Unit) {
+    MaterialTheme(content = content)
+}
