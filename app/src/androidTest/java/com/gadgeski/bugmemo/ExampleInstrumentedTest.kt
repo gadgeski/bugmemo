911123ad04e9b24d7de327a1f3821121f7d899da
gadgeski <xtwh.ce5bil@gmail.com>
2026-02-27 23:31:47 +0900

    fix: correct package name in ExampleInstrumentedTest

--- app/src/androidTest/java/com/gadgeski/bugmemo/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/gadgeski/bugmemo/ExampleInstrumentedTest.kt
@@ -2,7 +2,7 @@ package com.gadgeski.bugmemo
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
-import org.junit.Assert.assertEquals // ★ Changed: ワイルドカードをやめて個別 import に
+import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -17,6 +17,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.bugmemo", appContext.packageName)
+        assertEquals("com.gadgeski.bugmemo", appContext.packageName)
     }
 }
